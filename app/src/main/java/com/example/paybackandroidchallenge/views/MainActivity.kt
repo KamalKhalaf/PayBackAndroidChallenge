ae1d04c19248ee565f200c59cd6c0ac52831ce8a
kamal khalaf <kamal.farghali@procco.com>
2023-02-07 22:08:23 +0300

    Modularize the project with the data and domain layers.

--- app/src/main/java/com/example/paybackandroidchallenge/views/MainActivity.kt
+++ app/src/main/java/com/example/paybackandroidchallenge/views/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.paybackandroidchallenge
+package com.example.paybackandroidchallenge.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.paybackandroidchallenge.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
