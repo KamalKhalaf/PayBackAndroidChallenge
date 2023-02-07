81032cb238f5bde529c87f2fa0ffbddfe3e0a4fc
kamal khalaf <kamal.farghali@procco.com>
2023-02-07 23:51:00 +0300

    Add the Hilt library and use it to create the network module / create APP class

---
+++ app/src/main/java/com/example/paybackandroidchallenge/common/App.kt
@@ -0,0 +1,12 @@
+package com.example.paybackandroidchallenge.common
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * @Created by: Kamal.Farghali
+ * @Date: 07/02/2023 : 11:40 PM
+ */
+
+@HiltAndroidApp
+class App : Application()
---
+++ app/src/main/java/com/example/paybackandroidchallenge/di/NetworkModule.kt
@@ -0,0 +1,56 @@
+package com.example.paybackandroidchallenge.di
+
+import com.example.paybackandroidchallenge.BuildConfig
+import com.example.paybackandroidchallenge.common.ErrorInterceptor
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+/**
+ * @Created by: Kamal.Farghali
+ * @Date: 07/02/2023 : 11:20 PM
+ */
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private val logger: HttpLoggingInterceptor
+        get() {
+            val loggingInterceptor = HttpLoggingInterceptor()
+            if (BuildConfig.DEBUG) {
+                loggingInterceptor.apply { level = HttpLoggingInterceptor.Level.BODY }
+            }
+            return loggingInterceptor
+        }
+
+    @Singleton
+    @Provides
+    fun provideOkHttp(errorInterceptor: ErrorInterceptor) : OkHttpClient {
+        return OkHttpClient.Builder().apply {
+            connectTimeout(60, TimeUnit.SECONDS)
+            readTimeout(60, TimeUnit.SECONDS)
+            writeTimeout(60, TimeUnit.SECONDS)
+            addInterceptor(errorInterceptor)
+            addInterceptor(logger)
+        }.build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttp: OkHttpClient) : Retrofit {
+        return Retrofit.Builder().apply {
+            addConverterFactory(GsonConverterFactory.create())
+            client(okHttp)
+            baseUrl(BuildConfig.BASE_URL)
+        }.build()
+    }
+
+}
