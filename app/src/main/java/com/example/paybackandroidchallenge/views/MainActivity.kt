e1aed300d35703f43cc980f5bba53cb9f92df872
kamal khalaf <kamal.farghali@procco.com>
2023-02-08 09:33:22 +0300

    Create Base activity to handle the binding and  the configuration change

--- app/src/main/java/com/example/paybackandroidchallenge/views/MainActivity.kt
+++ app/src/main/java/com/example/paybackandroidchallenge/views/MainActivity.kt
@@ -1,35 +1,35 @@
 package com.example.paybackandroidchallenge.views
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
-import com.example.paybackandroidchallenge.R
+import com.example.paybackandroidchallenge.base.BaseActivity
+import com.example.paybackandroidchallenge.databinding.ActivityMainBinding
 import com.example.paybackandroidchallenge.viewmodel.ImagesViewModel
 import com.example.paybackandroidchallenge.viewmodel.ImagesViewStatus
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 
-
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>() {
 
     private val viewmodel: ImagesViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+    override fun initViews() {
         viewmodel.getImages("fruits")
         viewmodel.state
             .flowWithLifecycle(lifecycle, Lifecycle.State.CREATED)
             .onEach { status -> handleResponse(status) }
             .launchIn(lifecycleScope)
+    }
+
+    override fun inflateLayout(layoutInflater: LayoutInflater) = ActivityMainBinding.inflate(layoutInflater)
+
+    override fun initConfigurations() {
 
     }
 
@@ -37,19 +37,15 @@ class MainActivity : AppCompatActivity() {
         when (status) {
             is ImagesViewStatus.IsLoading ->
                 if (status.isLoading) {
-                    var xx = 4
                 }else {
-                    var xc = 5
                 }
 
             is ImagesViewStatus.ShowToast -> Toast.makeText(this, status.message ,Toast.LENGTH_LONG).show()
             is ImagesViewStatus.SuccessGetImages -> {
                 val response = status.response
-                var x = 3
             }
             is ImagesViewStatus.ErrorGetImages -> {
                 val response = status.error
-                var x = 3
             }
             else -> {
             }
